8e531fde255cce76c1899c554ec64d688ca69043
Marco Antonio Islas Cruz <markuz@islascruz.org>
2013-08-01 09:43:50 -0500

    Fixed primes, wasn't working as expected

--- prime.py
+++ prime.py
@@ -23,7 +23,7 @@ def is_prime(number):
     fnumber = float(number)
     prime = True
     root = math.sqrt(number)
-    if root % 1:
+    if math.ceil(root) % 1:
         prime = False
     else:
         maxi = math.ceil(root)
@@ -38,15 +38,18 @@ def is_prime(number):
 
 po = Pool(process_to_disppatch)
 c = time.time()
-results  = po.map(is_prime, (k for k in xrange(3,1000000)))
+results  = po.map(is_prime, (k for k in xrange(3,200)))
 print "Processing time was: ", time.time() - c
 
 # remove if you want to see the prime numbers :-)
-sys.exit()
-print len(results)
+#sys.exit()
+#print len(results)
 results.sort(None, lambda x: x[1])
 
+print repr([v for k,v in results if k])
+
 for prime, number in results:
+    #print prime, number
     if prime:
         print number, prime
 
