eeba485934faae06edf75064ed1e29cae29d0296
Marco Antonio Islas Cruz <markuz@islascruz.org>
2014-01-20 13:16:02 -0600

    Add osxnotification.sh, a bash call to osascript to send a notification in OS X

---
+++ osxnotification.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MESSAGE=$1
+TITLE=$2
+
+CMD="display notification \"$1\""
+if [ "x$TITLE" != "x" ]; then
+    CMD= "$CMD with title \"$title\"";
+fi
+
+echo "$CMD"
+
+osascript -e "$CMD"
