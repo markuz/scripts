40b94db11ba141cc134952ccb176096b184492bd
Marco Antonio Islas Cruz <markuz@islascruz.org>
2018-04-26 14:32:16 -0500

    Startftp

--- startftp.sh
+++ startftp.sh
@@ -1,1 +1,6 @@
-sudo launchctl load -w /System/Library/LaunchDaemons/ftp.plist
+#!/bin/bash
+if [ "x$1" == "xstop" ]; then
+    sudo launchctl unload -w /System/Library/LaunchDaemons/ftp.plist
+else
+    sudo launchctl load -w /System/Library/LaunchDaemons/ftp.plist
+fi
